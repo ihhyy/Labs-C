5d020b4db768849136d578c7a4f2307da85b891d
ihhy <ihhyman.ii@gmail.com>
2021-04-28 17:08:48 +0300

    lab4

---
+++ lab4/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace lab8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var calc = new CollectionType<int>();
+            calc.Sum(5, 5);
+        }
+    }
+
+    class CollectionType<T> : Methods, IShow<T> where T : struct
+    { 
+        List<T> list = new List<T>();
+
+        public void Add(T num)
+        {
+            list.Add(num);
+        }
+
+        public void Delete(T num)
+        {
+            list.Remove(num);
+        }
+
+        public void Show()
+        {
+            for(int i = 0; i < list.Count; i++)
+            {
+                Console.WriteLine(list[i]);
+            }
+        }
+    }
+}
