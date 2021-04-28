d77460c626a06f42909d374bd1c627602b3ba544
ihhy <ihhyman.ii@gmail.com>
2021-04-28 17:11:17 +0300

    lab11

---
+++ lab11/Ex4.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace lab12
+{
+    class Ex4
+    {
+        public string ex;
+
+        public Ex4(string _ex)
+        {
+            ex = _ex;
+        }
+
+        public void Display()
+        {
+            Console.WriteLine(ex);
+        }
+    }
+}
